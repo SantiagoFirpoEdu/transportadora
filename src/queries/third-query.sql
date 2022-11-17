972b03a77014b2110e4e5530fd37af0963bd0cfe
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2022-11-17 20:47:10 -0300

    Replaced pessoas_fisicas with pessoas_juridicas

--- src/queries/third-query.sql
+++ src/queries/third-query.sql
@@ -1,4 +1,6 @@
---Listar o nome de todos os clientes pessoa jurídica, acompanhados da quantidade de entregas solicitadas e do valor total de suas apólices de seguro, apenas para clientes do Rio Grande do Sul que tenham solicitado pelo menos 5 entregas.
+--Listar o nome de todos os clientes pessoa jurídica, acompanhados da
+-- quantidade de entregas solicitadas e do valor total de suas apólices de seguro, apenas para
+-- clientes do Rio Grande do Sul que tenham solicitado pelo menos 5 entregas.
 SELECT
     clientes.nome,
     COUNT(entregas.id_entrega) AS quantidade_entregas,
@@ -7,7 +9,7 @@ SELECT
 FROM
 (
     clientes
-    INNER JOIN pessoas_fisicas ON clientes.id_cliente = pessoas_fisicas.id_cliente
+    INNER JOIN pessoas_juridicas pessoa_juridica ON clientes.id_cliente = pessoa_juridica.id_cliente
     LEFT JOIN enderecos ON clientes.id_cliente = enderecos.id_cliente
     LEFT JOIN entregas ON entregas.id_cliente = clientes.id_cliente
     LEFT JOIN apolices ON apolices.id_cliente = clientes.id_cliente
